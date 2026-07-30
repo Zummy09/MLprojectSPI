c195fbb8a634335524af3a35786899bf26ce4af1
Sumit Shaw <sumit140809@gmail.com>
2026-07-30 22:42:30 +0530

    Data ingestion

--- src/logger.py
+++ src/logger.py
@@ -3,10 +3,13 @@ import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(os.path.dirname(logs_path), exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+# Only create the logs folder
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+
+# Path to the log file
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
